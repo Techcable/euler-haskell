d10ffc475870ed8724c3b8f93fcb5e09d0d5162b
Techcable <Techcable@techcable.net>
2020-09-08 12:16:18 -0700

    Use trial division to find primes
    Because of haskell weirdness this is way faster than
    my last implementation....

--- simple/Utils/Primes.hs
+++ simple/Utils/Primes.hs
@@ -1,31 +1,24 @@
 module Utils.Primes (
-    primesUntil,
+    primes,
     primeFactors
 ) where
 
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IntSet
+-- Had trouble figuring out how to avoid creating tons of lists
+--
+-- Based on this wiki page:
+-- http://wiki.haskell.org/Prime_numbers#Optimal_trial_division
+primes = 2 : oddprimes
+    where
+        oddprimes =  sieve [3,5..] 9 oddprimes
+    -- Pattern match `~` ensures that `future_primes` won't be eagerly evaluated
+    -- See page: https://wiki.haskell.org/Lazy_pattern_match
+        sieve (first_prime:remaining_primes) limit futures@ ~(future_test:remaining_futures)
+          -- If the start of the list is less than the current limit of known primes,
+          -- we've already tested and verified it's primes. It can simply be prepended to the
+          -- result list
+          | first_prime < limit = first_prime : sieve remaining_primes limit futures
+          -- Otherwise filter out the list of primes based on if it's not divisible by `future_test`
+          | otherwise =  sieve [x | x <- remaining_primes, x `rem` future_test /= 0]
+                                (head remaining_futures^2) remaining_futures
 
-dividesAny divisors target = any (\x -> target `mod` x == 0) divisors
-
--- NOTE: Internal method for prime sieve
-eliminatePrimes :: IntSet -> Int -> Int -> IntSet
-eliminatePrimes primeSet start stop
-    | start >= stop = primeSet -- Finished
-    -- Just skip to next number if we know `start` is not prime
-    | start `IntSet.notMember` primeSet =  eliminatePrimes primeSet (start + 1) stop
-    | otherwise = eliminatePrimes filteredPrimeSet (start + 1) stop -- Just continue eliminating
-        where primeSquared = ceiling $ sqrt $ fromIntegral start
-              filteredPrimeSet = primeSet `IntSet.difference` (IntSet.fromDistinctAscList $
-                takeWhile (<=stop) (map (\i -> start ^ 2 + start * i) [0..]))
-
-
-primesUntil :: Int -> [Int]
-
-primesUntil x = filter (\n -> IntSet.member n primeSet) [2..x]
-    where primeSet = eliminatePrimes (IntSet.fromDistinctAscList [2..x]) 2 x
-
-primeFactors :: Int -> [Int]
-primeFactors x = let
-    possibleFactors = primesUntil x
-    in filter (\fac -> x `mod` fac == 0) possibleFactors
+primeFactors target = [p | p <- (takeWhile (<=target) primes), target `rem` p == 0]
